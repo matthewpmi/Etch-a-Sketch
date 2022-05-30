284dbdd8c2c72a2b5c92e5646bae19cc3401676a
Matt <mpmekita@gmail.com>
2022-05-30 15:46:02 -0400

    create 16x16 grid

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Etch-a-Sketch
 Etch-a-Sketch web app
 In this project, you’ll be creating a pretty neat toy for your portfolio to flex your DOM manipulation skills. You’re going to build a browser version of something between a sketchpad and an Etch-A-Sketch.
+
+- create a 16x16 grid
+- set up hover animations to color in boxes
+- create buttons for different colors/erase function
+- create clear all button
+- maybe create options for different grid size
--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const container = document.querySelector('#container')
+function createGrid(v) {
+    for (let i=0; i<v; i++) {
+        const row = document.createElement('div');
+        row.className = 'row';
+        for(let j=1; j<=v; j++) {
+            let cell = document.createElement('div');
+            cell.className = 'gridSquare';
+            row.appendChild(cell);            
+        }
+        container.appendChild(row);
+    }
+}
+createGrid(16);
