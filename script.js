20ce9b9d23f9247c8b00b9d430512810931d24e4
Matt <mpmekita@gmail.com>
2022-06-01 15:40:35 -0400

    complete without styling, colors, or shading

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const container = document.querySelector('#container')
-const gridSize = prompt("Size")
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+"use strict";
+
 function createGrid(v) {
+    const container = document.querySelector('#container')
+    container.style.gridTemplateColumns = `repeat(${v}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${v}, 1fr)`
+    let removeCells = container.querySelectorAll('div');
+    removeCells.forEach((div) => div.remove())
     for (let i=0; i<v*v; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell';  
         container.appendChild(cell);        
         }
         
-        const cellColor = document.querySelectorAll('.cell')
-            for (let i=0; i<cellColor.length; i++) {
-            cellColor[i].addEventListener('mouseenter', () => cellColor[i].style.backgroundColor = 'black')
+    const cellColor = document.querySelectorAll('.cell')
+    for (let i=0; i<cellColor.length; i++) {
+        cellColor[i].addEventListener('mouseenter', () => cellColor[i].style.backgroundColor = 'black')
  }
 }
 
-createGrid(gridSize);
+createGrid(16);
+
+function changeSize(input) {
+    if(input >= 2 || input <=100) {
+    createGrid(input);
+    }
+    else {
+        console.log('bad number')
+    }
+}
 
